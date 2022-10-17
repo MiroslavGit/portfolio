70a1ad54ec0b4149fcc7b867ef46cb1be04653f5
MiroslavGit <83980350+MiroslavGit@users.noreply.github.com>
2022-10-17 16:48:50 +0200

    letters added but scale dont work

--- src/pages/Me.tsx
+++ src/pages/Me.tsx
@@ -1,18 +1,70 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import VerticalNav from "../components/VerticalNav";
 
 import "./me.css";
 
 function Me() {
+  const dataFetchedRef = useRef(false);
+
+  const addShowClassToLetters = () => {
+    const allLetters = Array.from(document.getElementsByClassName("pismeno"));
+
+    setTimeout(() => {
+      Array.from(allLetters).forEach((letter, index) => {
+        setTimeout(() => {
+          letter.classList.toggle("show");
+        }, index * 170);
+      });
+    }, 1000);
+  };
+
+  useEffect(() => {
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
+    addShowClassToLetters();
+  }, []);
+
   return (
     <div className="body-page">
       <VerticalNav />
-      <div className="main">
-        <h1>Ahoj</h1>
-        <h1>Ahoj</h1>
-        <h1>Ahoj</h1>
-        <h1>Ahoj</h1>
+
+      <div className="bouncing-div">
+        <div>
+          <span className="pismeno">H</span>
+          <span className="pismeno">i</span>
+          <span className="pismeno">,</span>
+        </div>
+        <div>
+          <span className="pismeno">I</span>
+          <span className="pismeno">'</span>
+          <span className="pismeno">m</span>
+          <span className="pismeno "> </span>
+          <span className="pismeno ">M</span>
+          <span className="pismeno ">i</span>
+          <span className="pismeno ">r</span>
+          <span className="pismeno ">o</span>
+          <span className="pismeno ">s</span>
+          <span className="pismeno ">l</span>
+          <span className="pismeno ">a</span>
+          <span className="pismeno ">v</span>
+          <span className="pismeno ">,</span>
+        </div>
+        <div>
+          <span className="pismeno ">w</span>
+          <span className="pismeno ">e</span>
+          <span className="pismeno ">b</span>
+          <span className="pismeno "> </span>
+          <span className=" pismeno ">d</span>
+          <span className=" pismeno ">e</span>
+          <span className=" pismeno ">v</span>
+          <span className=" pismeno ">e</span>
+          <span className=" pismeno ">l</span>
+          <span className=" pismeno ">o</span>
+          <span className=" pismeno ">p</span>
+          <span className=" pismeno ">e</span>
+          <span className=" pismeno ">r</span>
+        </div>
       </div>
     </div>
   );
