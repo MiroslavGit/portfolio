a327a05a15fa092b8e781488d23e2ed25507af02
MiroslavGit <83980350+MiroslavGit@users.noreply.github.com>
2022-10-19 08:55:11 +0200

    pages changes

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,29 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 
-function Contanc() {
+import "./me.css";
+
+function Cotact() {
+  const dataFetchedRef = useRef(false);
+
+  const addShowClassToLetters = () => {
+    const allLetters = Array.from(document.getElementsByClassName("pismeno"));
+
+    setTimeout(() => {
+      Array.from(allLetters).forEach((letter, index) => {
+        setTimeout(() => {
+          letter.classList.toggle("show");
+        }, index * 170);
+      });
+    }, 1000);
+  };
+
+  useEffect(() => {
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
+    addShowClassToLetters();
+  }, []);
+
   return (
     <motion.div
       initial={{ y: -500 }}
@@ -9,9 +31,28 @@ function Contanc() {
       exit={{ y: window.innerHeight, transition: { duration: 0.8 } }}
       className="body-page"
     >
-      <h1>contact</h1>
+      <div style={{ display: "flex", marginBottom: "50px" }}>
+        <div>
+          <div style={{ display: "flex" }}>
+            <span className="pismeno">C</span>
+            <span className="pismeno">o</span>
+            <span className="pismeno">n</span>
+            <span className="pismeno">t</span>
+            <span className="pismeno">a</span>
+            <span className="pismeno">c</span>
+            <span className="pismeno">t</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="popis-div">
+        <hr className="hr" />
+        <h2 className="popis">
+          Currently Part-time developer but soon Full stack developer ;)
+        </h2>
+      </div>
     </motion.div>
   );
 }
 
-export default Contanc;
+export default Cotact;
--- src/pages/MySkills.tsx
+++ src/pages/MySkills.tsx
@@ -1,7 +1,29 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 
+import "./me.css";
+
 function MySkills() {
+  const dataFetchedRef = useRef(false);
+
+  const addShowClassToLetters = () => {
+    const allLetters = Array.from(document.getElementsByClassName("pismeno"));
+
+    setTimeout(() => {
+      Array.from(allLetters).forEach((letter, index) => {
+        setTimeout(() => {
+          letter.classList.toggle("show");
+        }, index * 170);
+      });
+    }, 1000);
+  };
+
+  useEffect(() => {
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
+    addShowClassToLetters();
+  }, []);
+
   return (
     <motion.div
       initial={{ y: -500 }}
@@ -9,7 +31,28 @@ function MySkills() {
       exit={{ y: window.innerHeight, transition: { duration: 0.8 } }}
       className="body-page"
     >
-      <h1>My skills</h1>
+      <div style={{ display: "flex", marginBottom: "50px" }}>
+        <div>
+          <div style={{ display: "flex" }}>
+            <span className="pismeno">M</span>
+            <span className="pismeno">y</span>
+            <span className="pismeno">&nbsp;</span>
+            <span className="pismeno">s</span>
+            <span className="pismeno">k</span>
+            <span className="pismeno">i</span>
+            <span className="pismeno">l</span>
+            <span className="pismeno">l</span>
+            <span className="pismeno">s</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="popis-div">
+        <hr className="hr" />
+        <h2 className="popis">
+          Currently Part-time developer but soon Full stack developer ;)
+        </h2>
+      </div>
     </motion.div>
   );
 }
