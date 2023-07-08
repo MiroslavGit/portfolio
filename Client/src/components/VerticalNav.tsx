61f571f68ffceb9fe2e4728f28e9b9e4d2eea664
MiroslavGit <miroslav2022@gmail.com>
2023-07-08 20:52:33 +0200

    opravená navigácia a animácie

--- Client/src/components/VerticalNav.tsx
+++ Client/src/components/VerticalNav.tsx
@@ -8,11 +8,14 @@ function VerticalNav() {
   const navigate = useNavigate();
 
   const [navClass, setnavClass] = useState("");
+  const [hamburgerClass, setnhamburgerClass] = useState("");
+
   const [sectionClass, setSelectionCLass] = useState("");
   const [animationMaskClass, setAnimationMaskClass] = useState("");
 
   const setClasses = () => {
     navClass === "openMenu" ? setnavClass("") : setnavClass("openMenu");
+    navClass === "openMenu" ? setnhamburgerClass("") : setnhamburgerClass("activatehamburger");
     sectionClass === "openSection" ? setSelectionCLass("") : setSelectionCLass("openSection");
     navClass === "openMenu" ? setAnimationMaskClass("") : setAnimationMaskClass("animation-mask-color-after");
   };
@@ -20,70 +23,77 @@ function VerticalNav() {
   return (
     <>
       <nav className={navClass}>
-        <span id="tag" className="btn_container" onClick={setClasses}>
+        <span className={`btn_container ${hamburgerClass}`} onClick={setClasses}>
           <span></span>
           <span></span>
           <span></span>
         </span>
-        <ul className="parent">
-          <img
-            src={logo}
-            onClick={() => { setClasses(), navigate("/") }}
-            alt="Miroslav Hanisko"
-          />
-          <li
-            onClick={() => { setClasses(), navigate("/") }}
-          >
-            <a>
-              &nbsp;&nbsp;&nbsp;&nbsp; Home &nbsp;&nbsp;&nbsp;&nbsp;
-              <span className="underline"></span>
-              <span className={`animation-mask ${animationMaskClass}`}></span>
-            </a>
-          </li>
-          <li
-            onClick={() => { setClasses(), navigate("/mywork") }}
-          >
-            <a>
-              &nbsp;&nbsp;&nbsp;&nbsp; My Work &nbsp;&nbsp;&nbsp;&nbsp;
-              <span className="underline" ></span>
-              <span className={`animation-mask ${animationMaskClass}`}></span>
-            </a>
-          </li>
-          <li
-            onClick={() => { setClasses(), navigate("/myskills") }}
-          >
-            <a>
-              &nbsp;&nbsp;&nbsp;&nbsp; My Skills &nbsp;&nbsp;&nbsp;&nbsp;
-              <span className="underline"></span>
-              <span className={`animation-mask ${animationMaskClass}`}></span>
-            </a>
-          </li>
-          <li
-            onClick={() => { setClasses(), navigate("/contact") }}
-          >
-            <a>
-              &nbsp;&nbsp;&nbsp;&nbsp; Contact &nbsp;&nbsp;&nbsp;&nbsp;
-              <span className="underline" aria-hidden="true"></span>
-              <span className={`animation-mask ${animationMaskClass}`}></span>
-            </a>
-          </li>
-          <li
-            onClick={() => { setClasses(), navigate("/social") }}
-          >
-            <a>
-              &nbsp;&nbsp;&nbsp;&nbsp; Social &nbsp;&nbsp;&nbsp;&nbsp;
-              <span className="underline" aria-hidden="true"></span>
-              <span className={`animation-mask ${animationMaskClass}`}></span>
-            </a>
-          </li>
-        </ul>
+        <img
+          src={logo}
+          onClick={() => { setClasses(), navigate("/") }}
+          alt="Miroslav Hanisko"
+          className="logo"
+        />
+        <div className="ulContainer">
+          <ul className="parent">
+            <li
+              onClick={() => { setClasses(), navigate("/") }}
+            >
+              <a>
+                &nbsp;&nbsp;&nbsp;&nbsp; Home &nbsp;&nbsp;&nbsp;&nbsp;
+                <span className="underline"></span>
+                <span className={`animation-mask ${animationMaskClass}`}></span>
+              </a>
+            </li>
+            <li
+              onClick={() => { setClasses(), navigate("/mywork") }}
+            >
+              <a>
+                &nbsp;&nbsp;&nbsp;&nbsp; My Work &nbsp;&nbsp;&nbsp;&nbsp;
+                <span className="underline" ></span>
+                <span className={`animation-mask ${animationMaskClass}`}></span>
+              </a>
+            </li>
+            <li
+              onClick={() => { setClasses(), navigate("/myskills") }}
+            >
+              <a>
+                &nbsp;&nbsp;&nbsp;&nbsp; My Skills &nbsp;&nbsp;&nbsp;&nbsp;
+                <span className="underline"></span>
+                <span className={`animation-mask ${animationMaskClass}`}></span>
+              </a>
+            </li>
+            <li
+              onClick={() => { setClasses(), navigate("/contact") }}
+            >
+              <a>
+                &nbsp;&nbsp;&nbsp;&nbsp; Contact &nbsp;&nbsp;&nbsp;&nbsp;
+                <span className="underline" aria-hidden="true"></span>
+                <span className={`animation-mask ${animationMaskClass}`}></span>
+              </a>
+            </li>
+            <li
+              onClick={() => { setClasses(), navigate("/social") }}
+            >
+              <a>
+                &nbsp;&nbsp;&nbsp;&nbsp; Social &nbsp;&nbsp;&nbsp;&nbsp;
+                <span className="underline" aria-hidden="true"></span>
+                <span className={`animation-mask ${animationMaskClass}`}></span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </nav >
 
       <section
         className={sectionClass}
         onClick={() => { setClasses() }}
       >
-        <h1>AhojAhojAhojAhojAhojAhojAhojAhojAhojAhojAhojAhojAhojAhojAhojAhoj</h1>
+        <div className="sectionContent">
+          <div className="socialIcons">
+
+          </div>
+        </div>
       </section>
     </>
   );
